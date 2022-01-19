feaa7b005becc717fd4938fcfc6c8865a800dad7
pirate-datenscienticfic <shubhamyadav7122000@gmail.com>
2022-01-19 14:33:53 +0530

    Day-1 ML deployment with Flask

---
+++ Day-1/FlaskApp_LR.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 12:22:37 2022
+
+@author: Acer
+"""
+
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request
+import sklearn
+import joblib
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("home.html")
+
+
+@app.route('/predict', methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        print(request.form.get("alcohol"))
+        print(request.form.get("volatile acidity"))
+        print(request.form.get("sulphates"))
+        print(request.form.get("total sulfur dioxide"))
+        try:
+            alc = float(request.form.get("alcohol"))
+            vol = float(request.form.get("volatile acidity"))
+            sul = float(request.form.get("sulphates"))
+            dio = float(request.form.get("total sulfur dioxide"))
+
+            pred_arg = [alc, vol, sul, dio]
+            #print(pred_arg)
+            pred_arg = np.array(pred_arg)
+            pred_arg = pred_arg.reshape(1, -1)
+            
+            model = open("linear_regression_model.pkl", 'rb')
+            lr_model = joblib.load(model)
+            
+            
+            try:
+                model_pred = lr_model.predict(pred_arg)
+                model_pred = round(float(model_pred), 2)
+                
+            except Exception as e:
+                return str(e)
+            
+            
+        except ValueError:
+            return "Please Enter Valid Values"
+
+    return render_template("predict.html", prediction=model_pred)
+
+
+if __name__ == "__main__":
+    app.run()
